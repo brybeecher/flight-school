fae1845bb35e838a56a44cc6dd3869c3f1ae91bf
Bryan Beecher <bryan@revivedesignstudios.com>
2017-03-29 15:05:58 -0600

    fix build, add pipeline config

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,6 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    groan
     erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
   end
 
